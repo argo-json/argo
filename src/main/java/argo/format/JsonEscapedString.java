f6bfb598e949664f14e18ced3ca8706d6ecbea5e
Mark Slater <mark.slater@mail.com>
2017-07-05 20:55:44 +0000

    Replace regex based string escaping with manual string escaping to facilitate streaming of formatted json strings, and because it's likely to be faster too.

--- src/main/java/argo/format/JsonEscapedString.java
+++ src/main/java/argo/format/JsonEscapedString.java
@@ -10,95 +10,123 @@
 
 package argo.format;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 final class JsonEscapedString {
-
-    private static final Pattern ESCAPE_CHARACTERS_PATTERN = Pattern.compile("(\\\\|\"|[\\u0000-\\u001f])");
 
     private JsonEscapedString() {
     }
 
     static String escapeString(final String unescapedString) {
-        final Matcher matcher = ESCAPE_CHARACTERS_PATTERN.matcher(unescapedString);
-        final StringBuffer sb = new StringBuffer();
-        while (matcher.find()) {
-            final String match = matcher.group();
-            if ("\\".equals(match)) {
-                matcher.appendReplacement(sb, "\\\\\\\\");
-            } else if ("\"".equals(match)) {
-                matcher.appendReplacement(sb, "\\\\\\\"");
-            } else if ("\u0000".equals(match)) {
-                matcher.appendReplacement(sb, "\\\\u0000");
-            } else if ("\u0001".equals(match)) {
-                matcher.appendReplacement(sb, "\\\\u0001");
-            } else if ("\u0002".equals(match)) {
-                matcher.appendReplacement(sb, "\\\\u0002");
-            } else if ("\u0003".equals(match)) {
-                matcher.appendReplacement(sb, "\\\\u0003");
-            } else if ("\u0004".equals(match)) {
-                matcher.appendReplacement(sb, "\\\\u0004");
-            } else if ("\u0005".equals(match)) {
-                matcher.appendReplacement(sb, "\\\\u0005");
-            } else if ("\u0006".equals(match)) {
-                matcher.appendReplacement(sb, "\\\\u0006");
-            } else if ("\u0007".equals(match)) {
-                matcher.appendReplacement(sb, "\\\\u0007");
-            } else if ("\u0008".equals(match)) {
-                matcher.appendReplacement(sb, "\\\\b");
-            } else if ("\u0009".equals(match)) {
-                matcher.appendReplacement(sb, "\\\\t");
-            } else if ("\n".equals(match)) {
-                matcher.appendReplacement(sb, "\\\\n");
-            } else if ("\u000b".equals(match)) {
-                matcher.appendReplacement(sb, "\\\\u000b");
-            } else if ("\u000c".equals(match)) {
-                matcher.appendReplacement(sb, "\\\\f");
-            } else if ("\r".equals(match)) {
-                matcher.appendReplacement(sb, "\\\\r");
-            } else if ("\u000e".equals(match)) {
-                matcher.appendReplacement(sb, "\\\\u000e");
-            } else if ("\u000f".equals(match)) {
-                matcher.appendReplacement(sb, "\\\\u000f");
-            } else if ("\u0010".equals(match)) {
-                matcher.appendReplacement(sb, "\\\\u0010");
-            } else if ("\u0011".equals(match)) {
-                matcher.appendReplacement(sb, "\\\\u0011");
-            } else if ("\u0012".equals(match)) {
-                matcher.appendReplacement(sb, "\\\\u0012");
-            } else if ("\u0013".equals(match)) {
-                matcher.appendReplacement(sb, "\\\\u0013");
-            } else if ("\u0014".equals(match)) {
-                matcher.appendReplacement(sb, "\\\\u0014");
-            } else if ("\u0015".equals(match)) {
-                matcher.appendReplacement(sb, "\\\\u0015");
-            } else if ("\u0016".equals(match)) {
-                matcher.appendReplacement(sb, "\\\\u0016");
-            } else if ("\u0017".equals(match)) {
-                matcher.appendReplacement(sb, "\\\\u0017");
-            } else if ("\u0018".equals(match)) {
-                matcher.appendReplacement(sb, "\\\\u0018");
-            } else if ("\u0019".equals(match)) {
-                matcher.appendReplacement(sb, "\\\\u0019");
-            } else if ("\u001a".equals(match)) {
-                matcher.appendReplacement(sb, "\\\\u001a");
-            } else if ("\u001b".equals(match)) {
-                matcher.appendReplacement(sb, "\\\\u001b");
-            } else if ("\u001c".equals(match)) {
-                matcher.appendReplacement(sb, "\\\\u001c");
-            } else if ("\u001d".equals(match)) {
-                matcher.appendReplacement(sb, "\\\\u001d");
-            } else if ("\u001e".equals(match)) {
-                matcher.appendReplacement(sb, "\\\\u001e");
-            } else if ("\u001f".equals(match)) {
-                matcher.appendReplacement(sb, "\\\\u001f");
-            } else {
-                throw new RuntimeException("Coding failure in Argo: JSON String escaping matched a character [" + match + "] for which no replacement is defined.");
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < unescapedString.length(); i++) {
+            switch (unescapedString.charAt(i)) {
+                case '\u0000':
+                    stringBuilder.append("\\u0000");
+                    break;
+                case '\u0001':
+                    stringBuilder.append("\\u0001");
+                    break;
+                case '\u0002':
+                    stringBuilder.append("\\u0002");
+                    break;
+                case '\u0003':
+                    stringBuilder.append("\\u0003");
+                    break;
+                case '\u0004':
+                    stringBuilder.append("\\u0004");
+                    break;
+                case '\u0005':
+                    stringBuilder.append("\\u0005");
+                    break;
+                case '\u0006':
+                    stringBuilder.append("\\u0006");
+                    break;
+                case '\u0007':
+                    stringBuilder.append("\\u0007");
+                    break;
+                case '\u0008':
+                    stringBuilder.append("\\b");
+                    break;
+                case '\u0009':
+                    stringBuilder.append("\\t");
+                    break;
+                case '\n':
+                    stringBuilder.append("\\n");
+                    break;
+                case '\u000b':
+                    stringBuilder.append("\\u000b");
+                    break;
+                case '\u000c':
+                    stringBuilder.append("\\f");
+                    break;
+                case '\r':
+                    stringBuilder.append("\\r");
+                    break;
+                case '\u000e':
+                    stringBuilder.append("\\u000e");
+                    break;
+                case '\u000f':
+                    stringBuilder.append("\\u000f");
+                    break;
+                case '\u0010':
+                    stringBuilder.append("\\u0010");
+                    break;
+                case '\u0011':
+                    stringBuilder.append("\\u0011");
+                    break;
+                case '\u0012':
+                    stringBuilder.append("\\u0012");
+                    break;
+                case '\u0013':
+                    stringBuilder.append("\\u0013");
+                    break;
+                case '\u0014':
+                    stringBuilder.append("\\u0014");
+                    break;
+                case '\u0015':
+                    stringBuilder.append("\\u0015");
+                    break;
+                case '\u0016':
+                    stringBuilder.append("\\u0016");
+                    break;
+                case '\u0017':
+                    stringBuilder.append("\\u0017");
+                    break;
+                case '\u0018':
+                    stringBuilder.append("\\u0018");
+                    break;
+                case '\u0019':
+                    stringBuilder.append("\\u0019");
+                    break;
+                case '\u001a':
+                    stringBuilder.append("\\u001a");
+                    break;
+                case '\u001b':
+                    stringBuilder.append("\\u001b");
+                    break;
+                case '\u001c':
+                    stringBuilder.append("\\u001c");
+                    break;
+                case '\u001d':
+                    stringBuilder.append("\\u001d");
+                    break;
+                case '\u001e':
+                    stringBuilder.append("\\u001e");
+                    break;
+                case '\u001f':
+                    stringBuilder.append("\\u001f");
+                    break;
+                case '\\':
+                    stringBuilder.append("\\\\");
+                    break;
+                case '\"':
+                    stringBuilder.append("\\\"");
+                    break;
+                default:
+                    stringBuilder.append(unescapedString.charAt(i));
             }
         }
-        matcher.appendTail(sb);
-        return sb.toString();
+
+        return stringBuilder.toString();
     }
 
 }
