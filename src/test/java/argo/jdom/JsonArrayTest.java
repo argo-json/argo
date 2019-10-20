08c10d16cbf565d69f38223f5b652cdbfac5f197
Mark Slater <mark.slater@mail.com>
2019-10-20 20:52:19 +0000

    Accept iterator of JsonNodes as contents of an array, to make use of streams slicker.

--- src/test/java/argo/jdom/JsonArrayTest.java
+++ src/test/java/argo/jdom/JsonArrayTest.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2012 Mark Slater
+ *  Copyright  2019 Mark Slater
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
  *
- * 	http://www.apache.org/licenses/LICENSE-2.0
+ *  	http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
  */
 
 package argo.jdom;
@@ -15,6 +15,8 @@ import org.junit.Test;
 import java.util.LinkedList;
 import java.util.List;
 
+import static argo.jdom.JsonNodeFactories.number;
+import static java.util.Collections.emptyIterator;
 import static java.util.Collections.singletonList;
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
@@ -23,16 +25,16 @@ public final class JsonArrayTest {
 
     @Test
     public void testImmutability() {
-        final JsonNode baseJsonNode = JsonNodeFactories.number("0");
+        final JsonNode baseJsonNode = number("0");
         final List<JsonNode> baseElements = new LinkedList<>(singletonList(baseJsonNode));
         final JsonNode jsonArray = JsonNodeFactories.array(baseElements);
         assertEquals(1, jsonArray.getElements().size());
         assertEquals(baseJsonNode, jsonArray.getElements().get(0));
-        baseElements.add(JsonNodeFactories.number("1"));
+        baseElements.add(number("1"));
         assertEquals(1, jsonArray.getElements().size());
         assertEquals(baseJsonNode, jsonArray.getElements().get(0));
         try {
-            jsonArray.getElements().add(JsonNodeFactories.number("1"));
+            jsonArray.getElements().add(number("1"));
             fail("modifying the elements retrieved from a JsonArray should result in an UnsupportedOperationException");
         } catch (UnsupportedOperationException e) {
             assertThat(e.getMessage(), is(nullValue()));
@@ -43,18 +45,21 @@ public final class JsonArrayTest {
     @Test
     public void testEquals() {
         assertEquals(JsonNodeFactories.array(new LinkedList<>()), JsonNodeFactories.array(new LinkedList<>()));
-        assertEquals(JsonNodeFactories.array(singletonList(JsonNodeFactories.number("0"))), JsonNodeFactories.array(singletonList(JsonNodeFactories.number("0"))));
-        assertFalse(JsonNodeFactories.array(singletonList(JsonNodeFactories.number("0"))).equals(JsonNodeFactories.array(singletonList(JsonNodeFactories.number("1")))));
+        assertEquals(JsonNodeFactories.array(singletonList(number("0"))), JsonNodeFactories.array(singletonList(number("0"))));
+        assertEquals(JsonNodeFactories.array(singletonList(number("0")).iterator()), JsonNodeFactories.array(singletonList(number("0"))));
+        assertEquals(JsonNodeFactories.array(singletonList(number("0")).iterator()), JsonNodeFactories.array(singletonList(number("0")).iterator()));
+        assertNotEquals(JsonNodeFactories.array(singletonList(number("0"))), JsonNodeFactories.array(singletonList(number("1"))));
     }
 
     @Test
     public void testHashCode() {
         assertEquals(JsonNodeFactories.array(new LinkedList<>()).hashCode(), JsonNodeFactories.array(new LinkedList<>()).hashCode());
-        assertEquals(JsonNodeFactories.array(singletonList(JsonNodeFactories.number("0"))).hashCode(), JsonNodeFactories.array(singletonList(JsonNodeFactories.number("0"))).hashCode());
+        assertEquals(JsonNodeFactories.array(emptyIterator()).hashCode(), JsonNodeFactories.array(new LinkedList<>()).hashCode());
+        assertEquals(JsonNodeFactories.array(singletonList(number("0"))).hashCode(), JsonNodeFactories.array(singletonList(number("0"))).hashCode());
     }
 
     @Test
     public void testToString() {
-        assertThat(JsonNodeFactories.array(singletonList(JsonNodeFactories.number("0"))).toString(), is(not(nullValue())));
+        assertThat(JsonNodeFactories.array(singletonList(number("0"))).toString(), is(not(nullValue())));
     }
 }
