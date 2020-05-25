6592978e81f97ab13d1d8e1c53f03b932663f6a1
Mark Slater <mark.slater@mail.com>
2020-05-25 19:07:23 +0000

    No more double brace initialisation.

--- src/testInfrastructure/java/argo/staj/ElementTrackingStajParserMatcher.java
+++ src/testInfrastructure/java/argo/staj/ElementTrackingStajParserMatcher.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2012 Mark Slater
+ *  Copyright  2020 Mark Slater
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
  *
- * 	http://www.apache.org/licenses/LICENSE-2.0
+ *  	http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
  */
 
 package argo.staj;
@@ -18,23 +18,25 @@ import org.hamcrest.TypeSafeDiagnosingMatcher;
 import java.util.ArrayList;
 import java.util.Collection;
 
+import static java.util.Arrays.asList;
+
 public final class ElementTrackingStajParserMatcher extends TypeSafeDiagnosingMatcher<StajParser> {
 
     private final Collection<JsonStreamElement> actual = new ArrayList<>();
-    private final JsonStreamElement[] jsonStreamElements;
+    private final Collection<JsonStreamElement> expected;
 
     public static Matcher<StajParser> generatesElements(final JsonStreamElement... jsonStreamElements) {
         return new ElementTrackingStajParserMatcher(jsonStreamElements);
     }
 
-    private ElementTrackingStajParserMatcher(final JsonStreamElement... jsonStreamElements) {
-        this.jsonStreamElements = jsonStreamElements;
+    private ElementTrackingStajParserMatcher(final JsonStreamElement... expected) {
+        this.expected = asList(expected);
     }
 
     @Override
     protected boolean matchesSafely(final StajParser item, final Description mismatchDescription) {
         boolean matches = true;
-        for (JsonStreamElement jsonStreamElement : jsonStreamElements) {
+        for (JsonStreamElement jsonStreamElement : expected) {
             final boolean hasNextItem = item.hasNext();
             if (!hasNextItem) {
                 matches = false;
@@ -57,6 +59,6 @@ public final class ElementTrackingStajParserMatcher extends TypeSafeDiagnosingMa
     }
 
     public void describeTo(final Description description) {
-        description.appendText("a StajParser that generates the sequence ").appendValueList("", ", ", "", jsonStreamElements);
+        description.appendText("a StajParser that generates the sequence ").appendValueList("", ", ", "", expected);
     }
 }
