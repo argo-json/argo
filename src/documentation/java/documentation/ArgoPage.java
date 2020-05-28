b0104a382da564be96a52d284dde6dbd0ab79e4d
Mark Slater <mark.slater@mail.com>
2020-05-28 21:54:46 +0000

    Driveby.

--- src/documentation/java/documentation/ArgoPage.java
+++ src/documentation/java/documentation/ArgoPage.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2015 Mark Slater
+ *  Copyright  2020 Mark Slater
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
  *
- * 	http://www.apache.org/licenses/LICENSE-2.0
+ *  	http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
  */
 
 package documentation;
@@ -139,13 +139,13 @@ final class ArgoPage {
         return codeTag(xhtmlText(snippet));
     }
 
-    static BlockElement codeBlock(String someCode) {
+    static BlockElement<?> codeBlock(String someCode) {
         return divTag(
                 xhtmlText(someCode)
         ).withClass(className("code"));
     }
 
-    static InlineTag simpleNameOf(final Class clazz) {
+    static InlineTag simpleNameOf(final Class<?> clazz) {
         return codeTag(xhtmlText(clazz.getSimpleName()));
     }
 }
