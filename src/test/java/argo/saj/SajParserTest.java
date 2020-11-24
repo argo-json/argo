12ba543b25f7c1f9dbf30c9e6200d6e3e4c1a0b3
Mark Slater <mark.slater@mail.com>
2020-11-24 22:39:10 +0000

    Use toolchain to specify jvm dependency in build.

--- src/test/java/argo/saj/SajParserTest.java
+++ src/test/java/argo/saj/SajParserTest.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright  2019 Mark Slater
+ *  Copyright  2020 Mark Slater
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
  *
@@ -22,7 +22,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.fail;
 
-final class SajParserTest {
+final class SajParserTest { // NOPMD TODO this should be turned off in the rules
     private static final JsonListener BLACK_HOLE_LISTENER = new JsonListener() {
         public void startDocument() {
         }
