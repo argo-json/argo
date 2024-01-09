47d0344fd49c8d93dea0d4a55b18cc43e36e6699
Mark Slater <mark.slater@mail.com>
2024-01-09 18:39:32 +0000

    Note

--- src/main/java/argo/staj/JsonStreamException.java
+++ src/main/java/argo/staj/JsonStreamException.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2023 Mark Slater
+ *  Copyright 2024 Mark Slater
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
  *
@@ -24,7 +24,7 @@ public final class JsonStreamException extends RuntimeException {
         this.typedCause = cause;
     }
 
-    public void rethrowCause() throws IOException {
+    public void rethrowCause() throws IOException { // TODO this is a bit whiffy
         throw typedCause;
     }
 }
