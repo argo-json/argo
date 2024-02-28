b4ae0d21f46050ce796116d73585ed1d489f6412
Mark Slater <mark.slater@mail.com>
2024-02-28 11:15:54 +0000

    Upgrade svg2ico plugin 1.1 -> 1.2.

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,7 @@ plugins {
     id("me.champeau.jmh") version "0.7.2"
     id("com.github.spotbugs") version "6.0.7"
     id("io.github.gradle-nexus.publish-plugin") version "1.3.0"
-    id("com.gitlab.svg2ico") version "1.1"
+    id("com.gitlab.svg2ico") version "1.2"
     id("org.asciidoctor.jvm.convert") version "4.0.2"
 
     id("release.sourceforge")
