6e3e637a2a290c3f15890d4a311da7c40588384a
Mark Slater <mark.slater@mail.com>
2024-03-07 15:11:19 +0000

    Document deprecation.

--- src/main/java/argo/format/CompactJsonFormatter.java
+++ src/main/java/argo/format/CompactJsonFormatter.java
@@ -20,14 +20,25 @@ import static argo.JsonGenerator.JsonGeneratorStyle.COMPACT;
 
 /**
  * JsonFormat that formats JSON as compactly as possible.  Instances of this class can safely be shared between threads.
+ *
+ * @deprecated Replaced by {@link JsonGenerator}.
  */
 @SuppressWarnings("deprecation")
-@Deprecated public final class CompactJsonFormatter extends AbstractJsonFormatter { // TODO document deprecation
+@Deprecated public final class CompactJsonFormatter extends AbstractJsonFormatter {
 
     private static final JsonGenerator JSON_GENERATOR = new JsonGenerator().style(COMPACT);
 
     private final boolean fieldOrderNormalising;
 
+    /**
+     * Constructs a {@code JsonFormatter} that formats JSON as compactly as possible.
+     *
+     * @deprecated Replaced by {@link JsonGenerator#generate(JsonNode)} and {@link JsonGenerator#generate(Writer, JsonNode)}.
+     *   For example, use
+     *     {@code new JsonGenerator().style(COMPACT).generate(Writer, JsonNode) }
+     *   or
+     *     {@code new JsonGenerator().style(COMPACT).generate(JsonNode) }
+     */
     public CompactJsonFormatter() {
         this(false);
     }
@@ -40,6 +51,12 @@ import static argo.JsonGenerator.JsonGeneratorStyle.COMPACT;
      * Gets a {@code JsonFormatter} that formats JSON as compactly as possible, outputting the fields of objects in the order they were defined.
      *
      * @return a {@code JsonFormatter} that formats JSON as compactly as possible, outputting the fields of objects in the order they were defined.
+     *
+     * @deprecated Replaced by {@link JsonGenerator#generate(JsonNode)} and {@link JsonGenerator#generate(Writer, JsonNode)}.
+     *   For example, use
+     *     {@code new JsonGenerator().style(COMPACT).generate(Writer, JsonNode) }
+     *   or
+     *     {@code new JsonGenerator().style(COMPACT).generate(JsonNode) }
      */
     public static CompactJsonFormatter fieldOrderPreservingCompactJsonFormatter() {
         return new CompactJsonFormatter();
@@ -50,6 +67,12 @@ import static argo.JsonGenerator.JsonGeneratorStyle.COMPACT;
      * In the event that more than one field with the same name exists in an object, the relative order of those fields is preserved.
      *
      * @return a {@code JsonFormatter} that formats JSON as compactly as possible, outputting the fields of objects in alphabetic order.
+     *
+     * @deprecated Replaced by {@link JsonGenerator#generateWithFieldSorting(JsonNode)} and {@link JsonGenerator#generateWithFieldSorting(Writer, JsonNode)}.
+     *   For example, use
+     *     {@code new JsonGenerator().style(COMPACT).generateWithFieldSorting(Writer, JsonNode) }
+     *   or
+     *     {@code new JsonGenerator().style(COMPACT).generateWithFieldSorting(JsonNode) }
      */
     public static CompactJsonFormatter fieldOrderNormalisingCompactJsonFormatter() {
         return new CompactJsonFormatter(true);
--- src/main/java/argo/format/PrettyJsonFormatter.java
+++ src/main/java/argo/format/PrettyJsonFormatter.java
@@ -22,7 +22,7 @@ import static argo.JsonGenerator.JsonGeneratorStyle.PRETTY;
  * JsonFormat that formats JSON in a human-readable form.  Instances of this class can safely be shared between threads.
  */
 @SuppressWarnings("deprecation")
-@Deprecated public final class PrettyJsonFormatter extends AbstractJsonFormatter { // TODO document deprecation
+@Deprecated public final class PrettyJsonFormatter extends AbstractJsonFormatter {
 
     private static final JsonGenerator JSON_GENERATOR = new JsonGenerator().style(PRETTY);
 
@@ -30,6 +30,12 @@ import static argo.JsonGenerator.JsonGeneratorStyle.PRETTY;
 
     /**
      * Constructs a {@code JsonFormatter} that formats JSON in a human-readable form, outputting the fields of objects in the order they were defined.
+     *
+     * @deprecated Replaced by {@link JsonGenerator#generate(JsonNode)} and {@link JsonGenerator#generate(Writer, JsonNode)}.
+     *   For example, use
+     *     {@code new JsonGenerator().style(PRETTY).generate(Writer, JsonNode) }
+     *   or
+     *     {@code new JsonGenerator().style(PRETTY).generate(JsonNode) }
      */
     public PrettyJsonFormatter() {
         this(false);
@@ -43,6 +49,12 @@ import static argo.JsonGenerator.JsonGeneratorStyle.PRETTY;
      * Gets a {@code JsonFormatter} that formats JSON in a human-readable form, outputting the fields of objects in the order they were defined.
      *
      * @return a {@code JsonFormatter} that formats JSON in a human-readable form, outputting the fields of objects in the order they were defined.
+     *
+     * @deprecated Replaced by {@link JsonGenerator#generate(JsonNode)} and {@link JsonGenerator#generate(Writer, JsonNode)}.
+     *   For example, use
+     *     {@code new JsonGenerator().style(PRETTY).generate(Writer, JsonNode) }
+     *   or
+     *     {@code new JsonGenerator().style(PRETTY).generate(JsonNode) }
      */
     public static PrettyJsonFormatter fieldOrderPreservingPrettyJsonFormatter() {
         return new PrettyJsonFormatter();
@@ -53,6 +65,12 @@ import static argo.JsonGenerator.JsonGeneratorStyle.PRETTY;
      * In the event that more than one field with the same name exists in an object, the relative order of those fields is preserved.
      *
      * @return a {@code JsonFormatter} that formats JSON in a human-readable form, outputting the fields of objects in alphabetic order.
+     *
+     * @deprecated Replaced by {@link JsonGenerator#generateWithFieldSorting(JsonNode)} and {@link JsonGenerator#generateWithFieldSorting(Writer, JsonNode)}.
+     *   For example, use
+     *     {@code new JsonGenerator().style(PRETTY).generateWithFieldSorting(Writer, JsonNode) }
+     *   or
+     *     {@code new JsonGenerator().style(PRETTY).generateWithFieldSorting(JsonNode) }
      */
     public static PrettyJsonFormatter fieldOrderNormalisingPrettyJsonFormatter() {
         return new PrettyJsonFormatter(true);
