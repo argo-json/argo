0fe2596ea95882a2545507e5e3295e08014b0b4a
Mark Slater <mark.slater@mail.com>
2024-03-26 16:50:39 +0000

    Make SourceforgeReleaseTask work with maverick-synergy-client 3.1.0.

--- buildSrc/src/main/kotlin/release/SourceforgeReleaseTask.kt
+++ buildSrc/src/main/kotlin/release/SourceforgeReleaseTask.kt
@@ -11,15 +11,24 @@
 package release
 
 import com.sshtools.client.SshClient
+import com.sshtools.client.shell.ExpectShell
+import com.sshtools.client.tasks.ShellTask.ShellTaskBuilder
+import com.sshtools.client.tasks.UploadFileTask.UploadFileTaskBuilder
+import com.sshtools.common.ssh.Channel
+import com.sshtools.common.ssh.ChannelEventListener
 import org.gradle.api.DefaultTask
 import org.gradle.api.GradleException
+import org.gradle.api.file.RegularFile
 import org.gradle.api.file.RegularFileProperty
+import org.gradle.api.provider.Provider
 import org.gradle.api.tasks.InputFile
 import org.gradle.api.tasks.TaskAction
 import java.net.URI
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse
+import java.nio.ByteBuffer
+import java.nio.charset.StandardCharsets.US_ASCII
 
 abstract class SourceforgeReleaseTask : DefaultTask() {
 
@@ -36,38 +45,54 @@ abstract class SourceforgeReleaseTask : DefaultTask() {
     fun release() {
         val username = "${project.property("sourceforgeUser")},argo"
         val password = project.property("sourceforgePassword").toString().toCharArray()
-        retrying {
-            SshClient.SshClientBuilder.create()
-                .withHostname("shell.sourceforge.net")
-                .withPort(22)
-                .withUsername(username)
-                .withPassword(password)
-                .build()
-        }.use {
-            logger.info(it.executeCommand("create"))
-            logger.info(it.executeCommand("mkdir -p /home/frs/project/argo/argo/${project.version}"))
-        }
-        retrying {
-            SshClient.SshClientBuilder.create()
-                .withHostname("web.sourceforge.net")
-                .withPort(22)
-                .withUsername(username)
-                .withPassword(password)
-                .build()
-        }.use {
-            it.putFile(documentationTar.get().asFile, "/home/project-web/argo/documentation-${project.version}.tgz")
-            it.putFile(combinedJar.get().asFile, "/home/frs/project/argo/argo/${project.version}/argo-${project.version}.jar")
-            it.putFile(smallJar.get().asFile, "/home/frs/project/argo/argo/${project.version}/argo-small-${project.version}.jar")
-        }
-        retrying {
-            SshClient.SshClientBuilder.create()
-                .withHostname("shell.sourceforge.net")
-                .withPort(22)
-                .withUsername(username)
-                .withPassword(password)
-                .build()
-        }.use {
-            logger.info(it.executeCommand("mkdir -p /home/project-web/argo/${project.version} && tar -xvf /home/project-web/argo/documentation-${project.version}.tgz -C /home/project-web/argo/${project.version} && rm /home/project-web/argo/documentation-${project.version}.tgz && rm /home/project-web/argo/htdocs ; ln -s /home/project-web/argo/${project.version} /home/project-web/argo/htdocs"))
+        try {
+            retrying {
+                SshClient.SshClientBuilder.create()
+                    .withHostname("shell.sourceforge.net")
+                    .withPort(22)
+                    .withUsername(username)
+                    .withPassword(password)
+                    .build()
+            }.use {
+                it.execute("mkdir --parents /home/frs/project/argo/argo/${project.version}")
+            }
+            retrying {
+                SshClient.SshClientBuilder.create()
+                    .withHostname("web.sourceforge.net")
+                    .withPort(22)
+                    .withUsername(username)
+                    .withPassword(password)
+                    .build()
+            }.use {
+                it.executePut(documentationTar, "/home/project-web/argo/documentation-${project.version}.tgz")
+                it.executePut(combinedJar, "/home/frs/project/argo/argo/${project.version}/argo-${project.version}.jar")
+                it.executePut(smallJar, "/home/frs/project/argo/argo/${project.version}/argo-small-${project.version}.jar")
+            }
+            retrying {
+                SshClient.SshClientBuilder.create()
+                    .withHostname("shell.sourceforge.net")
+                    .withPort(22)
+                    .withUsername(username)
+                    .withPassword(password)
+                    .build()
+            }.use {
+                it.execute(
+                    "mkdir --parents /home/project-web/argo/${project.version}",
+                    "tar --extract --verbose --file=/home/project-web/argo/documentation-${project.version}.tgz --directory=/home/project-web/argo/${project.version}",
+                    "rm /home/project-web/argo/documentation-${project.version}.tgz",
+                    "rm /home/project-web/argo/htdocs",
+                    "ln --symbolic /home/project-web/argo/${project.version} /home/project-web/argo/htdocs"
+                )
+            }
+        } catch (e: SshExecuteRuntimeException) {
+            logger.error("Remote command execution failed", e)
+            e.commandOutput?.also {
+                logger.error("Command output:")
+                logger.error(it)
+            }
+            logger.error("Full session output:")
+            logger.error(e.sessionOutput)
+            throw e
         }
 
         val defaultDownloadUri =
@@ -94,8 +119,69 @@ abstract class SourceforgeReleaseTask : DefaultTask() {
 
     }
 
+    private fun SshClient.execute(vararg commands: String) {
+        class SshExecuteInnerRuntimeException(message: String, val commandOutput: String? = null) : RuntimeException(message)
+        val recordingChannelEventListener = RecordingChannelEventListener()
+        try {
+            runTask(
+                ShellTaskBuilder.create()
+                    .withClient(this)
+                    .onBeforeOpen { _, session ->
+                        session.addEventListener(recordingChannelEventListener)
+                        if (!session.executeCommand("create").waitFor(60_000).isDoneAndSuccess) {
+                            throw SshExecuteInnerRuntimeException("Failed to create shell")
+                        }
+                    }
+                    .onTask { task, _ ->
+                        val expectShell = ExpectShell(task)
+                        commands.forEach { command ->
+                            val shellProcess = expectShell.executeCommand(command, US_ASCII.name())
+                            shellProcess.drain()
+                            if (shellProcess.exitCode != 0) {
+                                val cause = when (val exitCode = shellProcess.exitCode) {
+                                    ExpectShell.EXIT_CODE_PROCESS_ACTIVE -> "process active"
+                                    ExpectShell.EXIT_CODE_UNKNOWN -> "exit code unknown"
+                                    else -> "exit code $exitCode"
+                                }
+                                throw SshExecuteInnerRuntimeException("Command $command failed with $cause", shellProcess.commandOutput)
+                            }
+                            logger.info(shellProcess.commandOutput)
+                        }
+                    }
+                    .build()
+            )
+        } catch (e: SshExecuteInnerRuntimeException) {
+            throw SshExecuteRuntimeException(e, e.commandOutput, recordingChannelEventListener.stdOut())
+        }
+    }
+
+    private fun SshClient.executePut(localFile: Provider<RegularFile>, remotePath: String) {
+        runTask(
+            UploadFileTaskBuilder.create()
+                .withClient(this)
+                .withLocalFile(localFile.get().asFile)
+                .withRemotePath(remotePath)
+                .build()
+        )
+    }
+
     private fun <T> retrying(block: () -> T) = generateSequence { runCatching(block) }
-        .filterIndexed { index, result -> index >= 5 || result.isSuccess }
-        .first()
-        .getOrThrow()
+            .filterIndexed { index, result -> index >= 5 || result.isSuccess }
+            .first()
+            .getOrThrow()
+
+    private class RecordingChannelEventListener : ChannelEventListener {
+        private val standardStringBuilder = StringBuilder()
+
+        override fun onChannelDataIn(channel: Channel, buffer: ByteBuffer) {
+            standardStringBuilder.append(US_ASCII.decode(buffer.asReadOnlyBuffer()))
+        }
+
+        fun stdOut(): String {
+            return standardStringBuilder.toString()
+        }
+    }
+
+    private class SshExecuteRuntimeException(cause: Exception, val commandOutput: String?, val sessionOutput: String) : RuntimeException(cause.message, cause)
+
 }
