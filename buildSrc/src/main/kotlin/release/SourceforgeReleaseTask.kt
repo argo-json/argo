aee290d15ad92e099794dbb3fceb4cc031027c88
Mark Slater <mark.slater@mail.com>
2024-03-26 17:17:36 +0000

    Remove unused import.

--- buildSrc/src/main/kotlin/release/SourceforgeReleaseTask.kt
+++ buildSrc/src/main/kotlin/release/SourceforgeReleaseTask.kt
@@ -27,7 +27,6 @@ import java.net.URI
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpRequest.BodyPublishers
-import java.net.http.HttpResponse
 import java.net.http.HttpResponse.BodyHandlers
 import java.nio.ByteBuffer
 import java.nio.charset.StandardCharsets.US_ASCII
