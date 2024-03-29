c8e4545f8151911e36495021ffe70357bb41f210
Mark Slater <mark.slater@mail.com>
2024-03-29 16:20:06 +0000

    Oops, fix build.

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,18 @@
  *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
  */
 
+import com.github.spotbugs.snom.SpotBugsTask
+
+/*
+ *  Copyright 2024 Mark Slater
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+ *
+ *  	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ */
+
 buildscript {
     repositories {
         mavenCentral()
@@ -257,6 +269,10 @@ tasks {
 
     spotbugsMain {
         excludeFilter = file("tools/spotbugs-main-filter.xml")
+    }
+
+    named<SpotBugsTask>("spotbugsDocs") {
+        excludeFilter = file("tools/spotbugs-docs-filter.xml")
     }
 
     spotbugsTest {
