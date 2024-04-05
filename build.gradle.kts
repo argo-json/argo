9512814a416d0a566e78c96ff4990ff68c9e177a
Mark Slater <mark.slater@mail.com>
2024-04-05 09:59:08 +0100

    Remove a duplicate dependency.

--- build.gradle.kts
+++ build.gradle.kts
@@ -79,7 +79,6 @@ testing {
                 implementation("org.hamcrest:hamcrest:2.2")
                 implementation("net.sourceforge.ickles:ickles:0.21")
                 implementation("commons-io:commons-io:2.16.0")
-                runtimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.2")
             }
         }
 
